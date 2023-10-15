394f2042a04e850f9c9c9857c017cd0173d3f22f
GuidoM197 <gmazzucco@fi.uba.ar>
2023-10-15 18:16:34 -0300

    Tercer commit TP2

--- TP2/Logica.py
+++ TP2/Logica.py
@@ -1,6 +1,12 @@
-def opcion_1(ruta):
-    palabras = input("Ingrese las palabras para contar entre contactos: ").lower().split()
-    guardado = input("Ingrese el archivo destino para guardar el reporte: ")
+from random import choice, choices
+
+def opcion_1(ruta, palabras, guardado):
+    """
+    Lee el archivo y genera un diccionario con todas las palabras y las veces que se repitieron, como "key" lleva el nombre del contacto
+    para asi identificar las veces que el contacto repitio las palabras.
+    """
+
+    palabras = palabras.lower().split()
     res = {}
 
     with open(ruta) as origen, open(guardado, "w") as destino:
@@ -24,4 +30,91 @@ def opcion_1(ruta):
             for i, j in res[contacto].items():
                 destino.write(f"{contacto}, {i}, {j}\n")
 
-print(opcion_1("Chat_de_Android_de_Friends.txt"))
+def recoleccion_de_datos(ruta): # Crea un diccionario que va juntando las palabras y las siguientes con sus ocurrencias.
+    """
+    Se lee linea por linea identificando los contactos y los mensajes que manda cada uno, luego de esto devuelve un diccionario
+    con las palabras que utlizaron y sus ocurrencias (Salvo las ocurrencias de las palabras iniciales).
+    """
+    
+    res = {}
+
+    with open(ruta) as f:
+
+        for linea in f:
+            mensaje = linea.lower().split()
+            contacto = mensaje[3]
+            mensaje = mensaje[4:]
+
+            if "<multimedia" in mensaje[0]: #No salta a la proxima iteracion, corregir!
+                continue
+
+            if ":" in contacto:
+                contacto = contacto[0].upper() + contacto[1:].rstrip(":")
+                res[contacto] = res.get(contacto, {})
+            else:
+                continue
+            
+            count = 0
+            for palabra in mensaje:
+
+                res[contacto][palabra] = res[contacto].get(palabra, {}) # AGrego la palabra donde estoy parado
+
+                count += 1
+
+                if count < len(mensaje):
+                    mensaje_siguiente = mensaje[count]
+
+                if not mensaje_siguiente in res[contacto][palabra] and mensaje_siguiente != palabra:
+                    res[contacto][palabra][mensaje_siguiente] = res[contacto][palabra].get(mensaje_siguiente, 0) + 1 #Agrego la palabra siguiente y le sumo su primera aparicion
+
+                if mensaje_siguiente in res[contacto][palabra] and mensaje_siguiente != palabra:
+                    res[contacto][palabra][mensaje_siguiente] += 1
+                
+        return res
+
+def generador_de_palabras(diccionario, contacto):
+    """
+    Recibe el diccionario de la funcion de "recoleccion_de_datos" y un contacto, luego filtra en base al contacto pasado para utilizar unicamente
+    sus palabras y asi generar un mensaje pseudo-aleatorio del mismo.
+    """
+
+    lista = []
+    peso = []
+    palabra_final = []
+
+    for nombre in diccionario.keys():
+        if nombre != contacto: continue
+
+        for palabra in diccionario[nombre]:
+            lista.append(palabra)
+        
+        while True:
+
+            palabra_actual = choice(lista)
+
+            palabra_final.append(palabra_actual)
+            lista.clear()
+            peso.clear()
+
+            for palabras, ocurrencias in diccionario[nombre][palabra_actual].items():
+                if palabras != "fin de texto":
+                    lista.append(palabras)
+                    peso.append(ocurrencias)                    
+
+                if palabras == "fin de texto":
+                    lista.append(".")
+
+            
+            if peso == [] or palabra_actual == ".": 
+                lista.remove(".")
+                break
+
+            palabra_actual = choices(lista, peso, k=1) #k=1 para que solo devuelva 1 puesto, el mas grande (boca).
+
+
+    return " ".join(palabra_final)
+
+def mostrar_contactos(diccionario):
+    for i, contacto in enumerate(diccionario.keys()):
+        print(i, "-" ,contacto)
+
--- TP2/aux.py
+++ TP2/aux.py
@@ -15,8 +15,10 @@ def recoleccion_de_datos(ruta): # Crea un diccionario que va juntando las palabr
             contacto = mensaje[3]
             mensaje = mensaje[4:]
 
-            if "<multimedia" in mensaje[0]: #No salta a la proxima iteracion, corregir!
-                continue
+            if "<multimedia" in mensaje:
+                mensaje = None
+                if mensaje == None:
+                    continue
 
             if ":" in contacto:
                 contacto = contacto[0].upper() + contacto[1:].rstrip(":")
@@ -24,25 +26,32 @@ def recoleccion_de_datos(ruta): # Crea un diccionario que va juntando las palabr
             else:
                 continue
             
-            count = 0
+            count = 1
             for palabra in mensaje:
 
-                res[contacto][palabra] = res[contacto].get(palabra, {}) # AGrego la palabra donde estoy parado
-
-                count += 1
+                res[contacto][palabra] = res[contacto].get(palabra, {}) # Agrego la palabra donde estoy parado
 
                 if count < len(mensaje):
                     mensaje_siguiente = mensaje[count]
 
-                if not mensaje_siguiente in res[contacto][palabra] and mensaje_siguiente != palabra:
-                    res[contacto][palabra][mensaje_siguiente] = res[contacto][palabra].get(mensaje_siguiente, 0) + 1 #Agrego la palabra siguiente y le sumo su primera aparicion
+                if count == len(mensaje):
+                    mensaje_siguiente = "fin de texto" #Identifica cuando corta el texto
+                
+                if mensaje_siguiente == "fin de texto" and mensaje_siguiente in res[contacto][palabra]: continue
 
                 if mensaje_siguiente in res[contacto][palabra] and mensaje_siguiente != palabra:
                     res[contacto][palabra][mensaje_siguiente] += 1
+
+                if not mensaje_siguiente in res[contacto][palabra] and mensaje_siguiente != palabra:
+                    res[contacto][palabra][mensaje_siguiente] = res[contacto][palabra].get(mensaje_siguiente, 0) + 1 #Agrego la palabra siguiente y le sumo su primera aparicion
+
+                count += 1
                 
         return res
+    
+#print(recoleccion_de_datos("/home/guido/Escritorio/Algoritmos y Programaciòn 1/TPS-Algo-1/TP2/ejemplo.txt"))
 
-def selector_de_palabras(diccionario, contacto):
+def generador_de_palabras(diccionario, contacto):
     """
     Recibe el diccionario de la funcion de "recoleccion_de_datos" y un contacto, luego filtra en base al contacto pasado para utilizar unicamente
     sus palabras y asi generar un mensaje pseudo-aleatorio del mismo.
@@ -57,7 +66,7 @@ def selector_de_palabras(diccionario, contacto):
 
         for palabra in diccionario[nombre]:
             lista.append(palabra)
-        
+
         while True:
 
             palabra_actual = choice(lista)
@@ -66,18 +75,15 @@ def selector_de_palabras(diccionario, contacto):
             lista.clear()
             peso.clear()
 
+            if len(palabra_final) == 10: break
+
             for palabras, ocurrencias in diccionario[nombre][palabra_actual].items():
-                if palabras != "fin de texto":
-                    lista.append(palabras)
-                    peso.append(ocurrencias)                    
+                if palabras == "fin de texto": break
 
-                if palabras == "fin de texto":
-                    lista.append(".")
+                lista.append(palabras)
+                peso.append(ocurrencias)
 
-            
-            if peso == [] or palabra_actual == ".": 
-                lista.remove(".")
-                break
+            if peso == []: break
 
             palabra_actual = choices(lista, peso, k=1) #k=1 para que solo devuelva 1 puesto, el mas grande (boca).
 
@@ -85,11 +91,14 @@ def selector_de_palabras(diccionario, contacto):
     return " ".join(palabra_final)
 
 def mostrar_contactos(diccionario):
+
     for i, contacto in enumerate(diccionario.keys()):
         print(i, "-" ,contacto)
 
-def op2(diccionario, contacto):
-    print(selector_de_palabras(diccionario, contacto))
-            
-            
-#op2("ejemplo.txt")
+def opcion_2(ruta, contacto):
+
+    datos = recoleccion_de_datos(ruta)
+
+    print(f"{contacto}: {generador_de_palabras(datos, contacto)}")
+
+opcion_2("Chat_de_Android_de_Friends.txt", "Monica")
